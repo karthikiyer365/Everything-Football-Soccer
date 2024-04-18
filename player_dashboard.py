8165be59d3d6a7ebd95301e9f3dc8ba69c0f7e0f
Karthik Iyer <68954031+karthikiyer365@users.noreply.github.com>
2024-04-18 00:09:46 -0400

    Update player_dashboard.py

--- player_dashboard.py
+++ player_dashboard.py
@@ -20,21 +20,27 @@ file_str_ending = 'male_players.csv'
 datasets = {}
 url = 'https://raw.githubusercontent.com/karthikiyer365/Football-Scout/Visualizations/'
 
+df_name = f'{url}FIFA15_male_players.csv'
+# print(f'FIFA{i}_{file_str_ending}')
+df = pd.read_csv(df_name)
+print(list(df.columns))
 
+df.dropna(axis=1, how='all')
+# df.drop(df.iloc[:, 78:], inplace = True, axis = 1)
+df.drop(['sofifa_id', 'player_url','long_name', 'dob','nationality', 'league_name', 'preferred_foot', 'international_reputation', 'weak_foot','skill_moves', 'work_rate', 'body_type', 'real_face', 'team_position','team_jersey_number','loaned_from','nation_position','nation_jersey_number','player_traits'], inplace = True, axis = 1)#sofifa_id, player_url,Long Name, DOB, Nationality, League name, 'preferred_foot', 'international_reputation', 'weak_foot','skill_moves', 'work_rate', 'body_type', 'real_face', 'team_position','team_jersey_number','loaned_from','nation_position','nation_jersey_number''player_traits'
+df.head().to_string()
+#%%
 for i in range(15,23):
     df_name = f'{url}FIFA{i}_{file_str_ending}'
-    # print(df_name)
-    try:
-        df = pd.read_csv(df_name)
-    except:
-        print(f'FIFA{i}_{file_str_ending}:  data cant load')
-    # print(df.head(5))
-
+    print(f'FIFA{i}_{file_str_ending}')
+    df = pd.read_csv(df_name)
+    
+    print(len(df.columns))
 
 for i in range(16,23):
     df_name = f'{url}FIFA{i}_fe{file_str_ending}'
-    try:
-        df = pd.read_csv(df_name)
-    except:
-        print(f'FIFA{i}_fe{file_str_ending}:  data cant load')
-    # print(df.head(5))
+    print(f'FIFA{i}_fe{file_str_ending}')
+    df = pd.read_csv(df_name)
+    print(len(df.columns))
+
+    
