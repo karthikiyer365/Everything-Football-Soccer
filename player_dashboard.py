f1a1f32458d7feb0e65f54e2f9a90ced0012f85a
Karthik Iyer <68954031+karthikiyer365@users.noreply.github.com>
2024-04-18 20:33:46 -0400

    Update player_dashboard.py

--- player_dashboard.py
+++ player_dashboard.py
@@ -1,46 +1,95 @@
-import dash
+'''Standard dependancies'''
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
-from dash import html, dcc
 import plotly.subplots as sp
-from dash.dependencies import Input, Output, State
-from scipy.fft import fft
-import math
-import base64
 import plotly.express as px
 import warnings
 warnings.filterwarnings('ignore')
+pd.set_option("display.max_columns",None)
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+'''Dash Dependencies'''
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output, State
+import math
 
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+'''Static part of code setup'''
 
 file_str_ending = 'male_players.csv'
-datasets = {}
 url = 'https://raw.githubusercontent.com/karthikiyer365/Football-Scout/Visualizations/'
+'''Understanding and cleaning data'''
 
 df_name = f'{url}FIFA15_male_players.csv'
-# print(f'FIFA{i}_{file_str_ending}')
 df = pd.read_csv(df_name)
-print(list(df.columns))
+# print(df.head(5))
 
-df.dropna(axis=1, how='all')
-# df.drop(df.iloc[:, 78:], inplace = True, axis = 1)
-df.drop(['sofifa_id', 'player_url','long_name', 'dob','nationality', 'league_name', 'preferred_foot', 'international_reputation', 'weak_foot','skill_moves', 'work_rate', 'body_type', 'real_face', 'team_position','team_jersey_number','loaned_from','nation_position','nation_jersey_number','player_traits'], inplace = True, axis = 1)#sofifa_id, player_url,Long Name, DOB, Nationality, League name, 'preferred_foot', 'international_reputation', 'weak_foot','skill_moves', 'work_rate', 'body_type', 'real_face', 'team_position','team_jersey_number','loaned_from','nation_position','nation_jersey_number''player_traits'
-df.head().to_string()
-#%%
+# Checking null percentage
+
+# for key, item in df.isna().sum().to_dict().items():
+#     if item > 0:
+#         print(key, round(item*100/len(df),2),'% null')
+
+
+def fetch_clean_df(gender, year):
+    if gender == 'M':
+        df_name = f'{url}FIFA{year}_{file_str_ending}'
+        df = pd.read_csv(df_name)
+        df.drop(['nation_logo_url', 'club_flag_url', 'club_logo_url', 'goalkeeping_speed',
+            'release_clause_eur', 'mentality_composure', 'player_traits',  'player_tags',
+            'release_clause_eur', 'nation_jersey_number', 'nation_position', 'nation_team_id',
+            'club_contract_valid_until', 'club_joined', 'club_loaned_from', 'club_jersey_number',
+            'ls', 'st', 'rs', 'lw', 'lf', 'cf', 'rf', 'rw', 'lam', 'cam', 'ram', 'lm', 'lcm', 'cm',
+            'rcm',	'rm', 'lwb', 'ldm', 'cdm', 'rdm', 'rwb', 'lb', 'lcb', 'cb', 'rcb', 'rb', 'gk', 'player_face_url',
+            'nation_flag_url'], inplace = True, axis = 1)
+
+        df['year'] = year
+        df['sex'] = 'M'
+        df['player_positions'] = df['player_positions'].str.split(', ')
+        df = df.explode('player_positions')
+        df.reset_index(inplace = True, drop = True)
+
+
+    else:
+        df_name = f'{url}FIFA{year}_fe{file_str_ending}'
+        df = pd.read_csv(df_name)
+        df.drop(['nation_logo_url', 'club_flag_url', 'club_logo_url', 'goalkeeping_speed',
+            'release_clause_eur', 'mentality_composure', 'player_traits',  'player_tags',
+            'release_clause_eur', 'nation_jersey_number', 'nation_position', 'nation_team_id',
+            'club_contract_valid_until', 'club_joined', 'club_loaned_from', 'club_jersey_number',
+            'ls', 'st', 'rs', 'lw', 'lf', 'cf', 'rf', 'rw', 'lam', 'cam', 'ram', 'lm', 'lcm', 'cm',
+            'rcm',	'rm', 'lwb', 'ldm', 'cdm', 'rdm', 'rwb', 'lb', 'lcb', 'cb', 'rcb', 'rb', 'gk', 'player_face_url',
+            'nation_flag_url'], inplace = True, axis = 1)
+        
+        df['year'] = year
+        df['sex'] = 'F'
+        df['player_positions'] = df['player_positions'].str.split(', ')
+        df = df.explode('player_positions')
+        df.reset_index(inplace = True, drop = True)
+
+    return df
+
+datasets = []
+length = 0
 for i in range(15,23):
-    df_name = f'{url}FIFA{i}_{file_str_ending}'
-    print(f'FIFA{i}_{file_str_ending}')
-    df = pd.read_csv(df_name)
-    
-    print(len(df.columns))
+    df = fetch_clean_df('M', i)
+    dataset_name = 'M'+str(i)
+    datasets.append(df)
+    length +=len(df)
+
 
 for i in range(16,23):
-    df_name = f'{url}FIFA{i}_fe{file_str_ending}'
-    print(f'FIFA{i}_fe{file_str_ending}')
-    df = pd.read_csv(df_name)
-    print(len(df.columns))
+    df = fetch_clean_df('F', i)
+    dataset_name = 'F'+str(i)
+    datasets.append(df)
+    length +=len(df)
 
-    
+AllPlayers = pd.concat(datasets, axis=0)
+
+
+print(length)
+print(len(AllPlayers))
+print(AllPlayers.sample())
